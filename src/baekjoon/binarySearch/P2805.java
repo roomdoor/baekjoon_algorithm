c040cb4882c3de367f9dab05f9acee01ece7f2b4
roomdoor <roomdoor.lee@gmail.com>
2021-11-01 13:53:07 +0900

    11/1

---
+++ src/baekjoon/binarySearch/P2805.java
@@ -0,0 +1,53 @@
+package baekjoon.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P2805 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        tree = new int[N];
+        int MAX = 0;
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+
+            if (tree[i] > MAX) MAX = tree[i];
+        }
+
+        BS(0, MAX, M);
+
+        System.out.println(answer);
+    }
+
+    public static int[] tree;
+    public static int answer = 0;
+
+    public static void BS(int min, int max, int M) {
+        if (min >= max) {
+            answer = min - 1;
+            return;
+        }
+
+        int mid = (min + max) / 2;
+        long sum = 0;
+
+        for (int treeHeight : tree)
+            if (treeHeight > mid) sum += treeHeight - mid;
+
+        if (sum < M)
+            BS(min, mid, M);
+
+        else
+            BS(mid + 1, max, M);
+
+    }
+}
