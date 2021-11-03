119a1a747bd0afa418f4a46f5a5e188a80748f82
roomdoor <roomdoor.lee@gmail.com>
2021-11-03 19:55:21 +0900

    11/3

--- src/dfsAndBfs/P1260.java
+++ src/dfsAndBfs/P1260.java
@@ -1,22 +1,44 @@
 package dfsAndBfs;
 
+import javafx.print.Collation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P1260 {
-
+    public static StringBuilder sb = new StringBuilder();
     public static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
     public static boolean[] isChecked;
 
-    public static void dfs() {
+    public static void dfs(int x) {
+        if (isChecked[x])
+            return;
 
+        isChecked[x] = true;
+        sb.append(x).append(" ");
+        for (int arr : list.get(x)) {
+            if (!isChecked[arr]) {
+                dfs(arr);
+            }
+        }
     }
 
-    public static void bfs() {
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(start);
+        isChecked[start] = true;
 
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            sb.append(x).append(" ");
+            for (int y : list.get(x))
+                if (!isChecked[y]) {
+                    isChecked[y] = true;
+                    queue.add(y);
+                }
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -26,13 +48,31 @@ public class P1260 {
         int m = Integer.parseInt(st.nextToken());
         int v = Integer.parseInt(st.nextToken());
 
+        for (int i = 0; i <= n; i++) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            list.add(arrayList);
+        }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(bf.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             list.get(x).add(y);
+            list.get(y).add(x);
         }
 
+        for (int i = 1; i <= n; i++)
+            Collections.sort(list.get(i));
+
+
+        isChecked = new boolean[n + 1];
+        dfs(v);
+
+        sb.append("\n");
+
+        isChecked = new boolean[n + 1];
+        bfs(v);
+
+        System.out.println(sb.toString());
     }
 }
