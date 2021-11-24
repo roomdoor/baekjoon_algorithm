f69baa49ec1461fbe2f4b9be5b5a1954900cbf35
roomdoor <roomdoor.lee@gmail.com>
2021-11-24 18:25:13 +0900

    11/24

--- src/shortestPath/P10217.java
+++ src/shortestPath/P10217.java
@@ -10,16 +10,14 @@ import java.util.StringTokenizer;
 public class P10217 {
     public static ArrayList<ArrayList<Airport>> map;
     public static boolean[] isVisited;
-    public static int[] timeCost;
+    public static long[] timeCost;
+    public static long[] montyCost;
     public static StringBuilder sb = new StringBuilder();
     public static int INF = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         int testCase = Integer.parseInt(bf.readLine());
-
         for (int i = 0; i < testCase; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int n = Integer.parseInt(st.nextToken());
@@ -28,10 +26,13 @@ public class P10217 {
 
             map = new ArrayList<>();
             isVisited = new boolean[n + 1];
-            timeCost = new int[n + 1];
+            timeCost = new long[n + 1];
+            montyCost = new long[n + 1];
+
             for (int j = 0; j <= n; j++) {
                 map.add(new ArrayList<>());
                 timeCost[j] = INF;
+                montyCost[j] = INF;
             }
 
             for (int j = 0; j < k; j++) {
@@ -43,55 +44,55 @@ public class P10217 {
 
                 map.get(start).add(new Airport(end, money, time));
             }
-            bfs(m);
+
+            bfs(m, n);
             if (timeCost[n] == INF) {
                 sb.append("Poor KCM").append("\n");
             } else {
                 sb.append(timeCost[n]).append("\n");
             }
-
         }
         System.out.println(sb.toString());
     }
 
-    public static void bfs(int m) {
+    public static void bfs(int m, int n) {
         PriorityQueue<Airport> queue = new PriorityQueue<>();
         queue.add(new Airport(1, 0, 0));
         timeCost[1] = 0;
-        isVisited[1] = true;
 
         while (!queue.isEmpty()) {
             Airport temp = queue.poll();
             int cur = temp.end;
-            int money = temp.moneyCost;
-            int time = temp.timeCost;
+            long money = temp.moneyCost;
+            long time = temp.timeCost;
+            if (cur == n) {
+                timeCost[n] = Math.min(time, timeCost[n]);
+                continue;
+            }
 
-            if (!isVisited[cur]) {
-                isVisited[cur] = true;
-                for (int i = 0; i < map.get(cur).size(); i++) {
-                    Airport next = map.get(cur).get(i);
-                    int nextAir = next.end;
-                    int nextMoneyCost = next.moneyCost;
-                    int nextTimeCost = next.timeCost;
 
-                    if (m >= money + nextMoneyCost && time + nextTimeCost < timeCost[nextAir]) {
-                        timeCost[nextAir] = time + nextTimeCost;
-                        queue.add(new Airport(nextAir, nextMoneyCost + money, nextTimeCost + time));
-                    }
+            for (Airport next : map.get(cur)) {
+                int nextAir = next.end;
+                long nextMoneyCost = next.moneyCost;
+                long nextTimeCost = next.timeCost;
+
+                if (m >= money + nextMoneyCost ) {
+                    timeCost[nextAir] = time + nextTimeCost;
+                    queue.add(new Airport(nextAir, nextMoneyCost + money, nextTimeCost + time));
+                    System.out.println(nextAir);
+                    System.out.println("money " + (money + nextMoneyCost) + " time " + (nextTimeCost + time));
                 }
             }
         }
-
-
     }
 
 
     private static class Airport implements Comparable<Airport> {
         int end;
-        int moneyCost;
-        int timeCost;
+        long moneyCost;
+        long timeCost;
 
-        public Airport(int end, int moneyCost, int timeCost) {
+        public Airport(int end, long moneyCost, long timeCost) {
             this.end = end;
             this.moneyCost = moneyCost;
             this.timeCost = timeCost;
@@ -101,10 +102,10 @@ public class P10217 {
         @Override
         public int compareTo(Airport o) {
             if (timeCost == o.timeCost) {
-                return moneyCost - o.moneyCost;
-            } else
-                return timeCost - o.timeCost;
+                return (int) (moneyCost - o.moneyCost);
+            } else {
+                return (int) (timeCost - o.timeCost);
+            }
         }
     }
-
 }
