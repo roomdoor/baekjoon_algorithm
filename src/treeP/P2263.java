8038572da49a2c9d2289c0ae68a7619921a000b5
roomdoor <roomdoor.lee@gmail.com>
2022-01-12 19:38:59 +0900

    1/12

--- src/treeP/P2263.java
+++ src/treeP/P2263.java
@@ -8,6 +8,8 @@ import java.util.StringTokenizer;
 public class P2263 {
     public static int[] inOrder;
     public static int[] postOrder;
+    public static int[] position;
+    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -15,6 +17,7 @@ public class P2263 {
         int n = Integer.parseInt(bf.readLine());
         inOrder = new int[n];
         postOrder = new int[n];
+        position = new int[n];
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < n; i++) {
@@ -23,8 +26,29 @@ public class P2263 {
 
         st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < n; i++) {
-            postOrder[i] = Integer.parseInt(st.nextT:1:1oken());
+            postOrder[i] = Integer.parseInt(st.nextToken());
+            position[i] =
         }
 
+        makePreOrder(0, n - 1, 0, n - 1);
+
+        System.out.println(sb.toString());
+    }
+
+
+    public static void makePreOrder(int IS, int IE, int PS, int PE) {
+        if (IS <= IE && PS <= PE) {
+            sb.append(postOrder[PE]).append(" ");
+
+            int root = IS;
+            for (int i = IS; i <= IE; i++) {
+                if (inOrder[i] == postOrder[PE]) {
+                    root = i;
+                    break;
+                }
+            }
+            makePreOrder(IS, root - 1, PS, PS + root - IS - 1);
+            makePreOrder(root + 1, IE, PS + root - IS, PE - 1);
+        }
     }
 }
