164a642ef9f01520e1bb78a60ea810f04aa46647
roomdoor <roomdoor.lee@gmail.com>
2022-01-21 15:17:46 +0900

    못품

---
+++ src/dynamic/P11066re.java
@@ -0,0 +1,61 @@
+package dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11066re {
+    public static int[][] page;
+//    public static int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(bf.readLine());
+            page = new int[k][k];
+
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < k; j++) {
+                page[j][j] = Integer.parseInt(st.nextToken());
+            }
+            int answer = pageSum(0, k - 1);
+            sb.append(answer).append("\n");
+
+//            print(k);
+
+        }
+
+        System.out.println(sb.toString());
+
+    }
+
+    public static int pageSum(int a, int b) {
+        if (a == b) {
+            return page[a][a];
+        }
+
+        int time1 = pageSum(a, b - 1) + pageSum(b, b);
+        int time2 = pageSum(a, a) + pageSum(a + 1, b);
+        return page[a][b] = Math.min(time1, time2);
+        
+
+    }
+
+    public static void print(int k) {
+        for (int j = 0; j < k; j++) {
+            for (int l = 0; l < k; l++) {
+                if (page[j][l] == 0) {
+                    System.out.print("   ");
+                } else {
+                    System.out.print(page[j][l] + " ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
