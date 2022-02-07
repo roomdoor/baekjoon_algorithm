6891d34b1ca27379992b6f53a7324ac3acf31296
roomdoor <roomdoor.lee@gmail.com>
2022-02-07 11:10:02 +0900

    2/7

--- src/dynamic/P7579re.java
+++ src/dynamic/P7579re.java
@@ -6,10 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class P7579 {
+public class P7579re {
     public static int[][] memory;
-    public static int[] DP;
-    public static boolean[] isVisited;
+    public static int[][] DP;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -17,44 +16,39 @@ public class P7579 {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        memory = new int[2][n];
+        memory = new int[2][n + 1];
 
         int maxM = 0;
         int maxC = 0;
         st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             memory[0][i] = Integer.parseInt(st.nextToken());
             maxM += memory[0][i];
         }
 
         st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             memory[1][i] = Integer.parseInt(st.nextToken());
             maxC += memory[1][i];
         }
 
-        isVisited = new boolean[maxC + 1];
-        DP = new int[maxC + 1];
-        Arrays.fill(DP, -1);
-        makeDP(0, maxC, maxM, n);
+        DP = new int[n + 1][maxC + 1];
 
-        int answer = Integer.MAX_VALUE;
-        for (int i = 0; i < DP.length; i++) {
-            if (DP[i] >= m && DP[i] != -1) {
-                answer = Math.min(answer, i);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= maxC; j++) {
+                if (j < memory[1][i]) {
+                    DP[i][j] = DP[i - 1][j];
+                } else {
+                    DP[i][j] = Math.max(DP[i - 1][j], DP[i - 1][j - memory[1][j]] + memory[0][j]);
+                }
             }
         }
-        System.out.println(answer);
-    }
 
-    public static void makeDP(int count, int c, int m, int n) {
-        if (DP[c] < m || DP[c] == -1) {
-            DP[c] = m;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= maxC ; j++) {
+                System.out.print(DP[i][j] + " ");
+            }
+            System.out.println();
         }
-
-        if (count == n) return;
-
-        makeDP(count + 1, c - memory[1][count], m - memory[0][count], n);
-        makeDP(count + 1, c, m, n);
     }
 }
