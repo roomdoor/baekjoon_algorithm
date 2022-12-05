b4cf0bb9c2b79996d7f5c36d98854fc05fa530c8
roomdoor <roomdoor.lee@gmail.com>
2022-12-05 12:43:51 +0900

    dfs, backtraking

--- src/P17136.java
+++ src/P17136.java
@@ -6,86 +6,74 @@ public class P17136 {
 
 	static String[][] map = new String[10][10];
 	static int[] paper = {0, 5, 5, 5, 5, 5};
+	static int result = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 		input();
 
-		check(0, 0, 5, 0);
+		check(0, 0, 0);
 
-		System.out.println(result);
+		if (result == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(result);
+		}
 	}
 
-	private static int check(int x, int y, int size, int result) {
-		boolean isMatched = true;
-
-		if (y + size > 10) {
-			x++;
-			y = 0;
-		}
-
-		if (x + size > 10) {
+	private static void check(int x, int y, int count) {
+		if (x >= 9 && y > 9) {
+			result = Math.min(result, count);
 			return;
 		}
 
-		out:
+		if (count >= result) {
+			return;
+		}
+
+		if (y > 9) {
+			check(x + 1, 0, count);
+			return;
+		}
+
+		if (map[x][y].equals("1")) {
+			for (int i = 5; i >= 1; i--) {
+				if (isPossible(x, y, i) && isMatch(x, y, i)) {
+					if (paper[i] <= 0) {
+						return;
+					}
+					paper[i]--;
+					stick(x, y, i, "0");
+					check(x, y + i, count + 1);
+					paper[i]++;
+					stick(x, y, i, "1");
+				}
+			}
+		} else {
+			check(x, y + 1, count);
+		}
+	}
+
+	private static void stick(int x, int y, int size, String paper) {
+		for (int i = x; i < x + size; i++) {
+			for (int j = y; j < y + size; j++) {
+				map[i][j] = paper;
+			}
+		}
+	}
+
+	private static boolean isPossible(int x, int y, int size) {
+		return x >= 0 && y >= 0 && x <= 10 - size && y <= 10 - size;
+	}
+
+	private static boolean isMatch(int x, int y, int size) {
 		for (int i = x; i < x + size; i++) {
 			for (int j = y; j < y + size; j++) {
 				if (!map[i][j].equals("1")) {
-					y = j + 1;
-					isMatched = false;
-					break out;
-				}
-			}
-		}
-
-		if (isMatched) {
-			if (paper[size] <= 0) {
-				boolean isCovered = false;
-
-				switch (size) {
-					case 4:
-						if (paper[2] >= 4) {
-							isCovered = true;
-							result += 4;
-							paper[2] -= 4;
-						}
-						break;
-
-					case 3:
-						if (paper[2] >= 1 && paper[1] >= 5) {
-							isCovered = true;
-							result += 6;
-							paper[2]--;
-							paper[1] -= 5;
-						}
-						break;
-
-					case 2:
-						if (paper[1] >= 4) {
-							isCovered = true;
-							result += 4;
-							paper[1] -= 4;
-						}
-						break;
-				}
-
-				if (!isCovered) {
-					result = -1;
 					return false;
 				}
-
-			} else {
-				paper[size]--;
 			}
-
-			for (int i = x; i < x + size; i++) {
-				for (int j = y; j < y + size; j++) {
-					map[i][j] = "0";
-				}
-			}
-			result++;
-			y = y + size;
 		}
+		return true;
 	}
 
 	private static void input() throws IOException {
