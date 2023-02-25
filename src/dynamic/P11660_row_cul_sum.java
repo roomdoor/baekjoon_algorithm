321b991826eb9cb63b709dfc7dc16897ac08b81c
roomdoor <roomdoor.lee@gmail.com>
2023-02-25 16:04:07 +0900

    DP DP re # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

---
+++ src/dynamic/P11660_row_cul_sum.java
@@ -0,0 +1,53 @@
+package dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P11660_row_cul_sum {
+
+	static int[][] map;
+	static int n;
+	static int m;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		input();
+
+		System.out.println(sb);
+	}
+
+
+	private static void input() throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n + 1][n + 1];
+
+		for (int i = 1; i < n + 1; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for (int j = 1; j < n + 1; j++) {
+				map[i][j] =
+					Integer.parseInt(st.nextToken())
+						+ map[i - 1][j] + map[i][j - 1] - map[i - 1][j - 1];
+			}
+
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int sx = Integer.parseInt(st.nextToken());
+			int sy = Integer.parseInt(st.nextToken());
+			int ex = Integer.parseInt(st.nextToken());
+			int ey = Integer.parseInt(st.nextToken());
+
+			sb.append(map[ex][ey] - map[sx - 1][ey] - map[ex][sy - 1] + map[sx - 1][sy - 1]);
+			sb.append("\n");
+		}
+	}
+
+}
