ff68c4e8b9aad05ad10e45a9ca7ba83987d1c1a1
roomdoor <roomdoor.lee@gmail.com>
2023-02-25 16:03:58 +0900

    DP # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

---
+++ src/dynamic/P11660_row_sum.java
@@ -0,0 +1,71 @@
+package dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11660_row_sum {
+
+	static int[][] map;
+	static int[][] culPoint;
+	static int n;
+	static int m;
+
+	public static void main(String[] args) throws IOException {
+		input();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < m; i++) {
+			sb.append(cul(culPoint[i][0], culPoint[i][1], culPoint[i][2], culPoint[i][3]));
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	private static int cul(int sx, int sy, int ex, int ey) {
+		int sum = 0;
+		if (sy == 0) {
+			for (int i = sx; i <= ex; i++) {
+				sum += map[i][ey];
+			}
+		} else {
+			for (int i = sx; i <= ex; i++) {
+				sum += map[i][ey] - map[i][sy - 1];
+			}
+		}
+
+		return sum;
+	}
+
+
+	private static void input() throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n][n];
+		culPoint = new int[m][4];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < n; j++) {
+				if (j != 0) {
+					map[i][j] = map[i][j - 1] + Integer.parseInt(st.nextToken());
+				} else {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(bf.readLine());
+			culPoint[i][0] = Integer.parseInt(st.nextToken()) - 1;
+			culPoint[i][1] = Integer.parseInt(st.nextToken()) - 1;
+			culPoint[i][2] = Integer.parseInt(st.nextToken()) - 1;
+			culPoint[i][3] = Integer.parseInt(st.nextToken()) - 1;
+		}
+	}
+
+}
